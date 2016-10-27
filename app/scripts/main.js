affab51c7d97a3821fa0c15ce4329ce0d6a56269
Zachariah Nagatani <Zachariah Nagatani>
2016-10-27 10:42:55 -0700

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+(function() {
+	$('#arrow-btn').click(function() {
+		$('body').animate({ scrollTop: $('#about').offset().top }, 'slow');
+	});
+
+	var path = window.location.pathname;
+
+	$(window).scroll(function() {
+		var nav = $('#navbar');
+		if ($(window).scrollTop() > $('#hero').height() - nav.height() ) {
+			nav.addClass('shrink');
+		} else {
+			nav.removeClass('shrink');
+		}
+	});		
+
+	$('.nav-link').click(function() {
+		var text = $(this).text();
+		if (text === 'About') {
+			$('body').animate({ scrollTop: $('#about').offset().top }, 'slow');
+		} else if (text === 'Portfolio') {
+			$('body').animate({ scrollTop: $('#portfolio').offset().top }, 'slow');
+		} else if (text === 'Contact') {
+			$('body').animate({ scrollTop: $('#contact').offset().top }, 'slow');
+		}
+	});
+
+	if (path !== '/index.html') {
+		$('#navbar').addClass('shrink').removeClass('navbar-fixed-top');
+	}
+})();
